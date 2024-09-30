0b0ee8ceb3c55148b3dd8aea23feec567811e494
Ilia Munaev <ilyamunaev@gmail.com>
2024-10-01 01:47:30 +0300

    Fix comments main.c

--- bm_blink/src/main.c
+++ bm_blink/src/main.c
@@ -12,16 +12,16 @@ void im_delay(volatile unsigned int ms)
 
 int main(void)
 {
-    // Set Pin 1_2 as output
+    // Set Pin 102 as output
     PORT1_PDR |= (1 << 2);
 
     while (1)
     {
-        // Set Pin 1_2 to high
+        // Set Pin 102 to high
         PORT1_PODR |= (1 << 2);   // Set bit 2 high
         im_delay(500);            // Delay 500ms
         
-        // Set Pin 1_2 to low
+        // Set Pin 102 to low
         PORT1_PODR &= ~(1 << 2);  // Clear bit 2 (set low)
         im_delay(500);            // Delay 500ms
     }
